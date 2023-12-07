694fc10e0286b839229bc286298ab1c1d3f809fc
kitnato <kitnato@pm.me>
2023-12-07 17:04:25 +0100

    feat: improved build and source.

--- source/index.ts
+++ source/index.ts
@@ -1,3 +1,8 @@
+import { addPluralRule } from "pluralize";
+
+addPluralRule(/ix$/i, "ices");
+addPluralRule(/us$/i, "i");
+
 export { generateArtifact } from "@locran/generate/generateArtifact";
 export { generateCreature } from "@locran/generate/generateCreature";
 export { generateLocation } from "@locran/generate/generateLocation";
